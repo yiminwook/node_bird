0de896ff743ddf1f935eae05854e53ed223ca872
yiminwook <grs0416@gmail.com>
2024-01-29 17:53:19 +0900

    Add: explore page

--- app/(afterLogin)/explore/page.tsx
+++ app/(afterLogin)/explore/page.tsx
@@ -1,3 +1,22 @@
+import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
+import Trend from "@/app/(afterLogin)/_component/Trend";
+import styles from "./explore.module.css";
+
 export default function Page() {
-  return <main>explore</main>;
+  return (
+    <main className={styles.main}>
+      <div className={styles.formZone}>
+        <SearchForm />
+      </div>
+      <div className={styles.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
+  );
 }
